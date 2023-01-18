e339331d52547518be779b67b5dd95c1f7ac8a12
shaileshm <shaileshmishra9907@gmail.com>
2023-01-18 17:48:37 +0530

    many to many

--- src/main/java/com/training/springboot/SpringbootApplication.java
+++ src/main/java/com/training/springboot/SpringbootApplication.java
@@ -1,5 +1,7 @@
 package com.training.springboot;
 
+import java.util.Arrays;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,11 +9,15 @@ import org.springframework.context.annotation.Bean;
 
 import com.training.springboot.model.Address;
 import com.training.springboot.model.Book;
+import com.training.springboot.model.Course;
 import com.training.springboot.model.Page;
+import com.training.springboot.model.Student;
 import com.training.springboot.model.User;
 import com.training.springboot.repository.AddressRepository;
 import com.training.springboot.repository.BookRepository;
+import com.training.springboot.repository.CourseRepository;
 import com.training.springboot.repository.PageRepository;
+import com.training.springboot.repository.StudentRepository;
 import com.training.springboot.repository.UserRepository;
 
 @SpringBootApplication
@@ -21,21 +27,51 @@ public class SpringbootApplication {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner oneToManyDemo(BookRepository bookRepo, PageRepository pageRepo) {
-		
-		return args ->{
 	
-			Book book = new Book("JAVA","Bates");
-			bookRepo.save(book);
+	@Bean
+	public CommandLineRunner oneToManyDemo(StudentRepository studRepo, CourseRepository courseRepo) {
+		
+		return args -> {
 			
-			pageRepo.save(new Page(1,"Introduction",book));
-			pageRepo.save(new Page(34,"Collection",book));
-			pageRepo.save(new Page(101,"Spring",book));
-			pageRepo.save(new Page(167,"Microservices",book));
+			Student student = new Student("Rohan", 24, "A+");
+			//Student student1 = new Student("Kishan", 22, "A");
+			
+			studRepo.save(student);
+			//studRepo.saveAll(Arrays.asList(student,student1));
+			
+			Course course1 = new Course("AI", 4, 25000);
+			Course course2 = new Course("ML", 4, 28000);
+			Course course3 = new Course("DevOps", 3, 20000);
+			
+			courseRepo.saveAll(Arrays.asList(course1,course2,course3));
+			
+			//Add courses for a student
+			
+			student.getCourses().addAll(Arrays.asList(course1,course2,course3));
+			
+			studRepo.save(student);
 		};
+		
 	}
 	
+	
+	
+	
+//	@Bean
+//	public CommandLineRunner oneToManyDemo(BookRepository bookRepo, PageRepository pageRepo) {
+//		
+//		return args ->{
+//	
+//			Book book = new Book("JAVA","Bates");
+//			bookRepo.save(book);
+//			
+//			pageRepo.save(new Page(1,"Introduction",book));
+//			pageRepo.save(new Page(34,"Collection",book));
+//			pageRepo.save(new Page(101,"Spring",book));
+//			pageRepo.save(new Page(167,"Microservices",book));
+//		};
+//	}
+//	
 	
 	/*
 	@Bean
